850d03a2f3fa8c4cf902e9e75446aeb26ddaad92
Sabin Kaphle <iamsabin35@gmail.com>
2025-01-11 23:31:10 +0545

    Delete tempCodeRunnerFile.py

--- 02_BallsCollision/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-def handleCollision(self, otherball):
-    #     distance = math.sqrt((self.x - otherball.x)**2 + (self.y - otherball.y)**2)  
-
-    #     if distance <= self.radius + otherball.radius:
-    #         if abs(self.x - otherball.x) > abs(self.y - oth
